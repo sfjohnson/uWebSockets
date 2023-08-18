cd7b1fd3cea7d73d0b04ff58dd10265c4447803e
Sam Johnson <sam@iroha.is>
2023-08-18 18:42:07 +1000

    Added Linux x86_64 build.

---
+++ linux-x64.mk
@@ -0,0 +1,32 @@
+AR = ar
+CC = gcc
+CXX = g++
+
+override CXXFLAGS += -Wpedantic \
+-Wall \
+-Wextra \
+-Wsign-conversion \
+-Wconversion \
+-std=c++2a \
+-Isrc \
+-IuSockets/src
+
+# WITH_LIBUV=1 builds with libuv as event-loop
+ifeq ($(WITH_LIBUV),1)
+	override LDFLAGS += -luv
+endif
+
+# WITH_ASAN builds with sanitizers
+ifeq ($(WITH_ASAN),1)
+	override CXXFLAGS += -fsanitize=address -g
+	override LDFLAGS += -lasan
+endif
+
+.PHONY: capi
+capi:
+	$(MAKE) -C uSockets
+	$(CXX) -O3 $(CXXFLAGS) -c capi/App.cpp -o capi.o $(LDFLAGS)
+	$(AR) rvs libuwebsockets-linux-x64.a capi.o uSockets/*.o
+
+all:
+	$(MAKE) capi
