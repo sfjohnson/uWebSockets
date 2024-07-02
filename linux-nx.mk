b019a491ee22f200756f9b41ff43228df4d3301a
Sam Johnson <sam@iroha.is>
2024-07-02 19:17:08 +0100

    trying docker

---
+++ linux-nx.mk
@@ -0,0 +1,34 @@
+DEVKITPRO = /opt/devkitpro
+
+ARCH = -march=armv8-a+crc+crypto -mtune=cortex-a57 -mtp=soft -fPIE
+
+override CFLAGS += -Wall -O2 -ffunction-sections -D__SWITCH__ $(ARCH)
+
+override LDFLAGS += -specs=$(DEVKITPRO)/libnx/switch.specs -g $(ARCH)
+
+override CXXFLAGS += -Wpedantic \
+-O2 \
+-ffunction-sections \
+$(ARCH) \
+-fno-rtti \
+-fno-exceptions \
+-Wall \
+-Wextra \
+-Wsign-conversion \
+-Wconversion \
+-std=c++2a \
+-Isrc \
+-IuSockets/src
+
+.PHONY: capi
+capi:
+	$(MAKE) -C uSockets -f nx.mk
+	$(CXX) -O3 $(CXXFLAGS) -c capi/App.cpp -o capi.o $(LDFLAGS)
+	$(AR) rvs libuwebsockets-nx.a capi.o uSockets/*.o
+
+all:
+	$(MAKE) capi
+
+clean:
+	$(MAKE) -C uSockets -f nx.mk clean
+	rm -f *.o
